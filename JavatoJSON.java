d97de5fb1a618bd9a310d313d644c18340b654c8
Superman641 <topaz989@yandex.ru>
2015-10-16 21:03:50 +0300

    new commit

---
+++ JavatoJSON.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package XMLtoJava;
+
+import org.json.simple.*;
+
+/**
+ *
+ * @author asus
+ */
+public class JavatoJSON 
+{
+   public static JSONObject makeJSON(Envelope envelope)
+   {
+      JSONObject json = new JSONObject();
+       
+      
+      return json;
+   }
+}
